84b9bd8400dfdea7f36ab13a2bcfa45ab13cb127
Matias Godoy <matigodoy93@hotmail.com>
2021-05-28 01:08:06 -0300

    Logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -5,7 +5,7 @@ import React from 'react';
 const Logo = () => {
     return (
         <Box d='flex' justifyContent='left' alignItems='center' w='100%' border= '1px solid #D9D9D9' >
-            <Image src='https://www.tailorbrands.com/wp-content/uploads/2020/07/mcdonalds-logo.jpg' alt='Logo' width='80px' ml='2%' p='15px' />
+            <Image src='https://www.estoes.me/assets/site/images/layout/esto-es.svg?v=2.0.1' alt='Logo' width='120px' ml='2%' p='15px' />
         </Box>
     );
 };
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,23 +1,42 @@
 import React from 'react';
-import { Box, Alert, Heading } from "@chakra-ui/react"
-import { Button, ButtonGroup } from "@chakra-ui/react"
+import {Link, useLocation} from 'react-router-dom'
+import { Box, Heading } from "@chakra-ui/react"
+import { Button } from "@chakra-ui/react"
 import Logo from "../Logo/Logo";
 
 const NavBar = () => {
+    const location = useLocation();
+
+    const pageTitle = () => {
+    
+        if(location.pathname === '/add') {
+            return 'Add Project'
+        } else if(location.pathname === '/edit') {
+            return 'Edit Project'
+        } else {
+            return 'My Projects'
+        }
+    }
+
     return (
-        <>
-                    
-            <Logo />
+        <Box backgroundColor='#fff'>
+
+            <Link to='/'>         
+                <Logo />
+            </Link>
 
             <Box w='100%' m='auto' mb='3%' d='flex' justifyContent='space-between' alignItems='center' boxShadow='inset 0px -1px 0px #D9D9D9'>
 
-                <Heading color='rgba(0, 0, 0, 0.85)' fontSize='20px' fontWeight='600' lineHeight='32px' ml='3%' >My Projects</Heading >
-
-                <Button colorScheme='red' m='10px 3% 10px 0' >+ Add project</Button>
-
+                <Heading color='rgba(0, 0, 0, 0.85)' fontSize='20px' lineHeight='32px' m='10px 3% 10px 3%'>{pageTitle()}</Heading >
+                {
+                    location.pathname === '/' &&
+                    <Link to='/add' style={{marginRight:'3%'}} >
+                        <Button colorScheme='red'  >+ Add project</Button>
+                    </Link>
+                }
             </Box>
             
-        </>
+        </Box>
     );
 };
 
